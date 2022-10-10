13c07dd35e2e28a91fea646d04712409063a47e0
munira <malsuhaim@outlook.sa>
2022-10-10 20:16:52 +0300

    Lab-Variables

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+'''
+Q1
+'''
 my_favourite_football_team = "AL-nasser"
+'''
+Q2
+'''
 x = 1
 y = 1.2
+'''
+Q3
+'''
 Value_One = value_two = "Done"
+'''
+Q4
+'''
 M_one = M_two = M_three = 50 , "Python_camp", "Second_Day"
+'''
+OR
+'''
+N_one , N_two , N_three = "Today is the " , 2 , " day in Python camp "
+
 
 print(my_favourite_football_team)
 print(x, y)
@@ -11,3 +28,4 @@ print(value_two)
 print(M_one)
 print(M_three)
 print(M_two)
+print(N_one , N_two, N_three)
