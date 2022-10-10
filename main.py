e001a260014dab498a2299668a0fd97c6bba94c2
munira <malsuhaim@outlook.sa>
2022-10-10 19:58:27 +0300

    Lab

---
+++ main.py
@@ -0,0 +1,13 @@
+my_favourite_football_team = "AL-nasser"
+x = 1
+y = 1.2
+Value_One = value_two = "Done"
+M_one = M_two = M_three = 50 , "Python_camp", "Second_Day"
+
+print(my_favourite_football_team)
+print(x, y)
+print(Value_One)
+print(value_two)
+print(M_one)
+print(M_three)
+print(M_two)
